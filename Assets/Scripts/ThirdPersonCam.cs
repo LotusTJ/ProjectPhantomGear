5e5248536725cfab790da673754c6696df23c0fa
LotusTJ <tejay20005@gmail.com>
2025-07-23 15:05:28 +0100

    Initial commit

---
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput; 
+
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObject;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; 
+    }
+    public void Update()
+    {
+        
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        //rotate player object 
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (inputDir != Vector3.zero) 
+        { playerObject.forward = Vector3.Slerp(playerObject.forward, inputDir.normalized, Time.deltaTime *rotationSpeed);  }
+
+        
+
+    }
+
+}
